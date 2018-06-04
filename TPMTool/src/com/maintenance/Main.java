bd71cc9529eb71b4a83dd565a7cc245d9878cafe
thaleradm <thaleradm@ilzwinihprod015.sgi2.root.magnapowertrain.com>
2018-06-04 07:46:25 +0200

    ...

--- TPMTool/src/com/maintenance/Main.java
+++ TPMTool/src/com/maintenance/Main.java
@@ -3,6 +3,10 @@ package com.maintenance;
 import java.io.File;
 import java.util.ResourceBundle;
 
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import com.maintenance.util.ApplicationProperties;
 import com.maintenance.view.tpm.MainFrame;
 
@@ -26,6 +30,16 @@ public class Main {
 	}
 
 	public Main() {
+		try {
+		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+		        if ("Nimbus".equals(info.getName())) {
+		            UIManager.setLookAndFeel(info.getClassName());
+		            break;
+		        }
+		    }
+		} catch (Exception e) {
+		    // If Nimbus is not available, you can set the GUI to another look and feel.
+		}
 
 		initProperties();
 
